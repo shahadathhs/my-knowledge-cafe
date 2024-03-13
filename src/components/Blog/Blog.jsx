cb157c05d270708a458ecb5f92e5f445d9f63da6
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-03-13 14:17:14 +0600

    project done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-// import { FaBookmark } from 'react-icons/fa';
+import { FaBookmark } from 'react-icons/fa';
 
 const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
     const { id, title, cover, author, author_img, posted_date, reading_time, hashtags } = blog;
@@ -16,10 +16,10 @@ const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
                 </div>
                 <div>
                     <span>{reading_time} min read</span>
-                    {/* <button
+                    <button
                         onClick={() => handleAddToBookmark(blog)}
                         className='ml-2 text-2xl text-red-600'
-                    ><FaBookmark></FaBookmark></button> */}
+                    ><FaBookmark></FaBookmark></button>
                 </div>
             </div>
             <h2 className="text-4xl">{title}</h2>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import profile from '../../assets/images/profile.png'
 
 const Header = () => {
-  return (
-    <div className='flex justify-between items-center p-4 border-b-2  max-w-7xl mx-auto'>
-      <h1 className='text-4xl'>Knowledge Cafe</h1>
-      <img src={profile} alt="" />
-    </div>
-  );
+    return (
+        <header className='flex justify-between items-center p-4 border-b-2  max-w-7xl mx-auto'>
+            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+        </header>
+    );
 };
 
 export default Header;
